909fc346a58a6eded172ebf8c822c82bd3f4f215
FabioVioli <fabiovioli84@gmail.com>
2023-01-23 17:18:13 +0100

    fabio

---
+++ src/app/components/create-post/create-post.component.ts
@@ -0,0 +1,47 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NzButtonSize } from 'ng-zorro-antd/button';
+
+@Component({
+  selector: 'app-create-post',
+  templateUrl: './create-post.component.html',
+  styleUrls: ['./create-post.component.scss']
+})
+
+export class CreatePostComponent implements OnInit {
+
+
+
+  print:boolean = false
+
+  postInterface = {
+    title: "",
+    post: "",
+    image: "",
+  }
+
+  @ViewChild('form',{static: true}) form!: NgForm;
+
+  posts:any = []
+
+  userPost:any = {
+    name: "",
+    title: "",
+    post: "",
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  submit(){
+
+    this.print = true;
+    console.log(this.form.value.formPost.title)
+    this.userPost.title = this.form.value.formPost.title;
+    this.userPost.post = this.form.value.formPost.post;
+  }
+}
